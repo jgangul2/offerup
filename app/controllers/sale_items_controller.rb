ddcc6621b4b27ba0530cc50c38212a3491c7801f
anonymous anonymous <jgangul2@illinois.edu>
2022-03-07 19:15:08 +0000

    Install Kaminari

--- app/controllers/sale_items_controller.rb
+++ app/controllers/sale_items_controller.rb
@@ -3,7 +3,7 @@ class SaleItemsController < ApplicationController
 
   # GET /sale_items
   def index
-    @sale_items = SaleItem.all
+    @sale_items = SaleItem.page(params[:page]).per(10)
   end
 
   # GET /sale_items/1
