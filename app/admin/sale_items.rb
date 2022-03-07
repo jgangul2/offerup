e3e12feea369c50e8eceb0c936bfc956a7f6730e
anonymous anonymous <jgangul2@illinois.edu>
2022-03-07 19:14:05 +0000

    Whitelist columns in SaleItem for ActiveAdmin

--- app/admin/sale_items.rb
+++ app/admin/sale_items.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register SaleItem do
 
+ permit_params :sale_price, :item_status, :seller_id, :buyer_id, :item_title, :location, :photo_link, :item_description, :item_category
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
