923cdd0dbb46e1488826f463120a62874052e2ba
anonymous anonymous <jgangul2@illinois.edu>
2022-03-07 19:14:20 +0000

    Display associated collections on show pages

--- app/controllers/sale_items_controller.rb
+++ app/controllers/sale_items_controller.rb
@@ -42,8 +42,14 @@ class SaleItemsController < ApplicationController
   # DELETE /sale_items/1
   def destroy
     @sale_item.destroy
-    redirect_to sale_items_url, notice: 'Sale item was successfully destroyed.'
+    message = "SaleItem was successfully deleted."
+    if Rails.application.routes.recognize_path(request.referrer)[:controller] != Rails.application.routes.recognize_path(request.path)[:controller]
+      redirect_back fallback_location: request.referrer, notice: message
+    else
+      redirect_to sale_items_url, notice: message
+    end
   end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
